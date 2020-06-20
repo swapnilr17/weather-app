0edf0352dac3ee3b9c7c1bc0cb05e6780126cacc
XavierGreek <xaviergreek2000@gmail.com>
2020-06-20 09:31:23 +0530

    EJS SERVER READY

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const app = express();
 const bodyparser = require('body-parser');
+const app = express();
+const ejs=require('ejs')
+app.set('view engine','ejs');
+app.get('/',function(req,res)
+{
+ res.render("form")
+});
 app.listen(3000);
 console.log("Server is running at port 3000");
