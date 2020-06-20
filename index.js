bf15574fda0d03368469964d5d941ea2656a47eb
XavierGreek <xaviergreek2000@gmail.com>
2020-06-20 10:57:56 +0530

    form

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const bodyparser = require('body-parser');
 const app = express();
 const ejs=require('ejs')
 app.set('view engine','ejs');
+app.use(express.static("public"));
 app.get('/',function(req,res)
 {
  res.render("form")
+});
+app.post('/weather',function(req,res)
+{
+ res.render("weather");
 });
 app.listen(3000);
 console.log("Server is running at port 3000");
